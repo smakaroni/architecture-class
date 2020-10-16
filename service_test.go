8876527f8c190662f9f042fd4c83a2c057d2fe6c
joakim persson <joakim.persson@findity.com>
2020-10-16 12:25:03 +0200

    added get test

--- service_test.go
+++ service_test.go
@@ -18,3 +18,19 @@ func TestPut(t *testing.T) {
 	ctrl.Finish()
 
 }
+
+func TestGet(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	acc := NewMockAccessor(ctrl)
+
+	p := Person{First: "Jokke"}
+	acc.EXPECT().Save(1, p).MinTimes(1).MaxTimes(1)
+
+	Put(acc, 1, p)
+
+	acc.EXPECT().Retrieve(1).Return(p)
+	Get(acc, 1)
+
+	ctrl.Finish()
+
+}
